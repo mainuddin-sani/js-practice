8e3e01042cc7bf5ab46ebf04070acd9151c1fd1b
mainuddin-sani <main12sani@gmail.com>
2022-03-18 00:26:25 +0600

    comparision

---
+++ comparison/comparison.js
@@ -0,0 +1,14 @@
+console.log(5<8);
+console.log(5>7);
+let firstName = 'mainuddin';
+let lastName = 'mainuddin';
+
+const math = 99;
+const bangla = 60;
+
+console.log(firstName.length <= lastName.length);
+console.log(firstName.length !== lastName.length);
+
+console.log(math>bangla);
+console.log(math<bangla);
+console.log(math!=bangla);
