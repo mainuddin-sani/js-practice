b2798e26c4690ea517d43727deaa59cdd4987d2b
mainuddin-sani <main12sani@gmail.com>
2022-03-17 23:59:33 +0600

    array

--- array/array.js
+++ array/array.js
@@ -1,1 +1,49 @@
-var sani = [1,2];
+// array
+
+var rollNumber = [1,2,3,4,5,6];
+let index = rollNumber.indexOf(9);
+rollNumber.push(59);
+rollNumber.push(100);
+for(var i = 0; i < rollNumber.length; i++){
+    console.log(rollNumber);
+}
+console.log(rollNumber);
+
+
+// string array
+
+var nameInfo = ['sani', 'man', 'kalam', 'jamal'];
+var nameResult = nameInfo[0];
+var nameResultIndex = nameInfo.indexOf('mani');
+console.log(nameResultIndex);
+console.log(nameResult);
+nameInfo.push('mala');
+nameInfo.push('jala');
+console.log(nameInfo);
+
+//  push pop
+
+let nameOfArray = ['java', 'pyton', 'javascript', 'C++', 'C#'];
+nameOfArray.push('heads');
+nameOfArray.push('name');
+nameOfArray.push('typescript');
+nameOfArray.push('react');
+nameOfArray.push('react Native');
+console.log(nameOfArray);
+
+
+// empty array
+
+let bdPlayer = [];
+bdPlayer.push('Masrafee');
+bdPlayer.push('Sakib');
+bdPlayer.push('mushfiq');
+bdPlayer.push('Rubel');
+bdPlayer.push('Taskin');
+bdPlayer.push('Tamim');
+bdPlayer.push('Riyad');
+console.log(bdPlayer);
+
+
+
+
