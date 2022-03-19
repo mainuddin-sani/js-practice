7bb5e77352c50464e6e2389cc78bc861f7ecd8cb
mainuddin-sani <main12sani@gmail.com>
2022-03-19 22:42:37 +0600

    practice

--- function/function.js
+++ function/function.js
@@ -9,3 +9,46 @@ function getTaka(taka){
 
 var setTaka = 400;
 getTaka(setTaka);
+
+// add, multiple, sustract ,  moduleus , divide 
+
+// multiple pera meeter
+
+//  add funtion
+function addNumber(num1, num2){
+    console.log(num1);
+    var addResult = num1 + num2;
+    return addResult;
+};
+
+var grandTotal = addNumber(2, 3);
+console.log(grandTotal);
+
+// multiple funtion
+
+function multipleNumber(multi1, multi2){
+    var total = multi1 * multi2;
+    return total;
+};
+
+var multiGrand = multipleNumber(40, 2);
+console.log(multiGrand);
+
+// deivid
+function divideNumber(divide1, divide2){
+    var totalFal = divide1 / divide2;
+    return totalFal;
+};
+
+var totalDivide = divideNumber(500, 10);
+console.log(totalDivide);
+
+// moudleus 
+
+function moduleusNumber(module1, module2){
+    var vagSesh = module2 % module1;
+    return vagSesh;
+};
+
+var totalfal = moduleusNumber(20, 43);
+console.log(totalfal);
---
+++ function/object.js
@@ -0,0 +1,34 @@
+// object declear
+
+var car = {
+    name: "car",
+    model: "Toyta",
+    color: 'red',
+    caka: 4,
+    window: 4,
+}
+console.log(car);
+
+var computer = {
+    brand: 'Hp',
+    proccesor: 'Intel i6',
+    memory: '216gb',
+    harddDisk: '1tb',
+}
+
+console.log(computer);
+
+// how to set object property
+
+var objectProperty = computer.brand;
+
+// 3 type object set
+
+computer.brand = "lenevo"
+var setComputerProccessor = "proccesor";
+computer[setComputerProccessor] = "gigabyte";
+
+car['name'] = 'lamburginiy',
+
+// console.log(objectProperty);
+console.log(car);
