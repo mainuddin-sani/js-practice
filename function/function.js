8bc20023ef7d421ed1a13d01599f85064bc025b3
mainuddin-sani <main12sani@gmail.com>
2022-03-19 21:07:09 +0600

    function practice

---
+++ function/function.js
@@ -0,0 +1,11 @@
+// simple function decleartion
+
+function getTaka(taka){
+    console.log('amar kace ' + taka + ' acay');
+    var chips = 10;
+    var getChips = taka / chips;
+    console.log(getChips);
+}
+
+var setTaka = 400;
+getTaka(setTaka);
